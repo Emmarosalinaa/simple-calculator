71edee4d073314c43de1a6451502e00905cc4ffb
Emmarosalinaa <66561978+Emmarosalinaa@users.noreply.github.com>
2020-07-23 14:15:42 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,98 @@
+const numbers = document.querySelectorAll('.number');
+
+const calculatorScreen = document.querySelector('.calculator-screen');
+const updateScreen = (number) => {
+    calculatorScreen.value = number
+};
+
+numbers.forEach((number) => {
+    number.addEventListener('click', (event) => {
+        inputNumber(event.target.value)
+        updateScreen(currentNumber)
+    });
+});
+
+let prevNumber = ' ';
+let calculationOperator = ' ';
+let currentNumber = '0';
+
+const inputNumber = (number) => {
+    if(currentNumber === '0') {
+        currentNumber = number
+    } else {
+        currentNumber += number
+    }
+};
+
+const inputOperator = (operator) => {
+    if (calculationOperator === ' ') {
+        prevNumber = currentNumber
+    }
+    calculationOperator = operator
+    currentNumber = ' '
+};
+
+const operators = document.querySelectorAll('.operator');
+operators.forEach((operator) => {
+    operator.addEventListener('click', (event) => {
+        inputOperator(event.target.value)
+    });
+});
+
+const equalSign = document.querySelector('.equal-sign');
+equalSign.addEventListener('click', () => {
+    calculate()
+    updateScreen(currentNumber)
+});
+
+const calculate = () => {
+    let result = ' '
+    switch(calculationOperator) {
+        case "+":
+            result = prevNumber + currentNumber
+            break
+        case "-":
+            result = prevNumber - currentNumber
+            break
+        case "*":
+            result = prevNumber * currentNumber
+            break;
+        case "/":
+            result = prevNumber / currentNumber
+            break
+        default:
+            return;
+    };
+    currentNumber = result
+    calculationOperator = ' '
+};
+
+const AllClears = document.querySelector('.allClear');
+AllClears.addEventListener('click', () => {
+    clearAll()
+    updateScreen(currentNumber)
+});
+
+const clearAll = () => {
+    prevNumber = ' '
+    calculationOperator = ' '
+    currentNumber = '0'
+};
+
+const decimal = document.querySelector('.decimal');
+decimal.addEventListener('click', (event) => {
+    inputDecimal(event.target.value)
+    updateScreen(currentNumber)
+});
+
+inputDecimal = (dot) => {
+    if(currentNumber.includes('.')) {
+        return
+    }
+    currentNumber += dot
+};
+
+const percentage = document.querySelector('.percentage')
+percentage.addEventListener('click', (event) => {
+    
+})
